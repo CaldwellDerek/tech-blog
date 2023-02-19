742f66f18050ba6dba0bc113e4d4966e56db5f39
Derek Caldwell <djohncaldwell@gmail.com>
2023-02-19 13:49:19 -0800

    updated db and seeds to include a post title

--- models/Post.js
+++ models/Post.js
@@ -4,6 +4,13 @@ const sequelize = require('../config/connection');
 class Post extends Model {};
 
 Post.init({
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [1, 64]
+        }
+    },
     post: {
         type: DataTypes.STRING,
         allowNull: false,
--- seeds/index.js
+++ seeds/index.js
@@ -22,42 +22,52 @@ const userData = [
 
 const postData = [
     {
+        title: "User Title",
         post:"User 1's first post",
         user_id: 1
     },
     {
+        title: "User Title",
         post:"Some random stuff User 1 is talking about",
         user_id: 1
     },
     {
+        title: "User Title",
         post:"User 1 decided to talk about some more random stuff",
         user_id: 1
     },
     {
+        title: "User Title",
         post:"User 2's first post",
         user_id: 2
     },
     {
+        title: "User Title",
         post:"User 2 just found some cool stuff to share",
         user_id: 2
     },
     {
+        title: "User Title",
         post:"User 3's first post",
         user_id: 3
     },
     {
+        title: "User Title",
         post:"User 3 brings some cool stuff to the table",
         user_id: 3
     },
     {
+        title: "User Title",
         post:"User 3 is asking for help about a new topic they found",
         user_id: 3
     },
     {
+        title: "User Title",
         post:"User 3 posting about a topic that interests them",
         user_id: 3
     },
     {
+        title: "User Title",
         post:"User 4's first post",
         user_id: 4
     },
