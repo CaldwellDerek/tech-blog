030fee55a4595df265809e080faa74a9ca62b97d
Derek Caldwell <djohncaldwell@gmail.com>
2023-02-22 18:43:02 -0800

    added links for comment page

--- public/js/comments.js
+++ public/js/comments.js
@@ -1,3 +1,48 @@
+const linkContainer = document.querySelector(".nav-links");
+
+const homeListItem = document.createElement("li");
+const homeLink = document.createElement("a");
+homeLink.setAttribute("href", "#");
+homeLink.setAttribute("class", "home");
+homeLink.textContent="Home";
+homeListItem.append(homeLink);
+
+const dashboardListItem = document.createElement("li");
+const dashboardLink = document.createElement("a");
+dashboardLink.setAttribute("href", "#");
+dashboardLink.setAttribute("class", "dashboard");
+dashboardLink.textContent="Dashboard";
+dashboardListItem.append(dashboardLink);
+
+const logoutListItem = document.createElement("li");
+const logoutLink = document.createElement("a");
+logoutLink.setAttribute("href", "#");
+logoutLink.setAttribute("class", "logout");
+logoutLink.textContent="Logout";
+logoutListItem.append(logoutLink);
+
+linkContainer.appendChild(homeListItem);
+linkContainer.appendChild(dashboardListItem);
+linkContainer.appendChild(logoutListItem); 
+
+homeLink.addEventListener("click", (e)=> {
+    e.preventDefault();
+    location.href="/";
+})
+
+dashboardLink.addEventListener("click", (e)=> {
+    e.preventDefault();
+    location.href="/dashboard";
+})
+
+logoutLink.addEventListener("click", async (e)=> {
+    e.preventDefault();
+
+    const logout = await fetch("/api/users/logout");
+
+    location.href="/login";
+})
+
 document.querySelector("#new-comment-form").addEventListener("submit", async (e)=> {
     e.preventDefault();
     const comment = document.querySelector("#comment").value;
